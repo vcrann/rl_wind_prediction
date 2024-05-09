513fc72d64b95a70a05b7978b97b11a6088b9e98
Verdon Crann <verdon.crann@manchester.ac.uk>
2024-05-09 19:18:43 +0100

    added keras version

---
+++ plot_keras.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import keras
+from keras import layers
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/training_data.csv")
+
+uas_state_data = df.iloc[:, 3:]  # X
+wind_data = df.iloc[:, 1:3]  # Y
+
+ss = StandardScaler()
+mm = MinMaxScaler()
+
+state_ss = ss.fit_transform(uas_state_data)
+wind_data_mm = mm.fit_transform(wind_data)
+
+state_ss = np.expand_dims(state_ss, 1)
+
+model = keras.saving.load_model("models/keras/model_keras_2.keras")
+predicted_wind = model.predict(state_ss)
+
+predicted_wind = mm.inverse_transform(predicted_wind)
+
+true_wind = mm.inverse_transform(wind_data_mm)
+
+
+plt.figure(figsize=(10, 6))  # plotting
+plt.axvline(x=1800, c="r", linestyle="--")  # size of the training set
+
+plt.plot(true_wind[:, 0], label="Anemometer Windspeed [m/s]")  # actual plot
+plt.plot(predicted_wind[:, 0], label="Predicted Windspeed [m/s]")  # predicted plot
+plt.title("Wind Speed Estimation")
+
+# plt.plot(dataY_plot, label="Actual Data")  # actual plot
+# plt.plot(data_predict, label="Predicted Data")  # predicted plot
+plt.title("Wind Speed Prediction")
+plt.legend()
+
+plt.figure(figsize=(10, 6))  # plotting
+plt.axvline(x=1800, c="r", linestyle="--")  # size of the training set
+plt.plot(true_wind[:, 1], label="Anemometer Wind Direction [°]")  # actual plot
+plt.plot(predicted_wind[:, 1], label="Predicted Wind Direction [°]")  # predicted plot
+plt.title("Wind Direction Estimation")
+plt.legend()
+plt.show()
---
+++ train_keras.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import keras
+from keras import layers
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import numpy as np
+import matplotlib.pyplot as plt
+
+# expected input data shape: (batch, timesteps, data_dim)
+data_dim = 6
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, data_dim)))
+# model.add(layers.LSTM(420, return_sequences=True, input_shape=(None, data_dim)))
+model.add(layers.LSTM(420, return_sequences=True))
+model.add(layers.LSTM(180, return_sequences=True))
+model.add(
+    layers.LSTM(90, return_sequences=True, dropout=0.96)
+)  # Check this droput, seems high?
+model.add(layers.LSTM(48, activation="relu"))
+model.add(layers.Dense(2))
+# model.compile(optimizer="adam", loss="mse", metrics=["accuracy"], learning_rate=4e-5)
+optimizer = keras.optimizers.RMSprop(learning_rate=4e-5)
+model.compile(optimizer=optimizer, loss="mse", metrics=["accuracy"])
+# print(model.summary())
+
+df = pd.read_csv("data/training_data.csv")
+
+uas_state_data = df.iloc[:, 3:]  # X
+wind_data = df.iloc[:, 1:3]  # Y
+
+ss = StandardScaler()
+mm = MinMaxScaler()
+
+state_ss = ss.fit_transform(uas_state_data)
+wind_data_mm = mm.fit_transform(wind_data)
+
+state_train = state_ss[:1800, :]
+state_test = state_ss[1800:, :]
+
+wind_train = wind_data_mm[:1800, :]
+wind_test = wind_data_mm[1800:, :]
+
+# state_train = np.reshape(state_train, (int(batch), 50, 6))
+
+state_train = np.expand_dims(state_train, 1)
+state_test = np.expand_dims(state_test, 1)
+
+
+training = model.fit(
+    state_train,
+    wind_train,
+    epochs=1500,
+    batch_size=16,
+    verbose=2,
+    validation_data=(state_test, wind_test),
+)
+
+plt.plot(training.history["loss"])
+plt.plot(training.history["val_loss"])
+plt.title("train vs validation loss")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.legend(["train", "validation"], loc="upper right", ncol=2)
+
+plt.show()
+
+model.save("models/keras/model_keras_2.keras")
