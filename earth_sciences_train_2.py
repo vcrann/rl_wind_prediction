15fcd99fe0f020a2942f266610c2c2cca0712652
Verdon Crann <verdon.crann@manchester.ac.uk>
2024-05-28 18:28:58 +0100

    added training for worst day

---
+++ earth_sciences_train_2.py
@@ -0,0 +1,89 @@
+import numpy as np
+import random
+import keras
+from keras import layers
+from data_generator import DataGenerator
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+lookback = np.load(
+    "data/earth_sciences_anemometer/training_data/worst_day_X_40_10.npy"
+)  # Lookback
+lookforward = np.load(
+    "data/earth_sciences_anemometer/training_data/worst_day_Y_40_10.npy"
+)  # Prediction
+
+lookback_window = 40
+prediction_window = 10
+
+# Test train split
+test_train_split = 0.8
+train_split_index = int(lookback.shape[0] * test_train_split)
+
+lookback_train = lookback[:train_split_index]
+lookback_validate = lookback[train_split_index:]
+lookforward_train = lookforward[:train_split_index]
+lookforward_validate = lookforward[train_split_index:]
+
+# Normalise data
+mm = (
+    MinMaxScaler()
+)  # As output is the same as input, we can use the same scaler (I think!)
+# TODO try normalising the output data separately
+
+lookback_train_mm = mm.fit_transform(
+    lookback_train.reshape(-1, lookback_train.shape[-1])
+).reshape(lookback_train.shape)
+lookback_validate_mm = mm.transform(
+    lookback_validate.reshape(-1, lookback_validate.shape[-1])
+).reshape(lookback_validate.shape)
+
+# lookback_train_mm = mm.fit_transform(lookback_train)
+# lookback_validate_mm = mm.transform(lookback_validate)
+# lookforward_train_mm = mm.transform(lookforward_train)
+# lookforward_validate_mm = mm.transform(lookforward_validate)
+
+# Shuffle training data
+p = np.random.permutation(lookback_train_mm.shape[0])
+lookback_train_mm = lookback_train_mm[p]
+# lookforward_train_mm = lookforward_train_mm[p]
+lookforward_train = lookforward_train[p]
+
+data_dim = 3
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, data_dim)))
+# model.add(layers.LSTM(420, return_sequences=True, input_shape=(None, data_dim)))
+model.add(layers.LSTM(420, return_sequences=True))
+model.add(layers.LSTM(180, return_sequences=True))
+model.add(
+    layers.LSTM(90, return_sequences=True, dropout=0.96)
+)  # Check this droput, seems high?
+model.add(layers.LSTM(60, activation="relu"))  # changed from 48
+model.add(layers.Dense(data_dim * prediction_window))
+model.add(layers.Reshape((prediction_window, data_dim)))
+# model.compile(optimizer="adam", loss="mse", metrics=["accuracy"], learning_rate=4e-5)
+optimizer = keras.optimizers.RMSprop(learning_rate=4e-5)
+model.compile(optimizer=optimizer, loss="mse", metrics=["accuracy"])
+
+
+# print(training_data_generator[0][0].shape)
+
+training = model.fit(
+    lookback_train_mm,
+    lookforward_train,
+    epochs=1500,
+    batch_size=16,
+    verbose=2,
+    validation_data=(lookback_validate_mm, lookforward_validate),
+)
+
+model.save("models/wind_prediction/env_sci_model_4.keras")
+
+plt.plot(training.history["loss"])
+plt.plot(training.history["val_loss"])
+plt.title("train vs validation loss")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.legend(["train", "validation"], loc="upper right", ncol=2)
+
+plt.show()
---
+++ setup_training_data.py
@@ -0,0 +1,44 @@
+import math
+import numpy as np
+
+raw_data = np.load("data/earth_sciences_anemometer/worst_day.npy")  # shape: (x, 3)
+
+lookback_window = 40
+prediction_window = 10
+
+# output_size = raw_data.shape[0] - lookback_window - prediction_window + 1
+
+output_size = math.floor(raw_data.shape[0] / (lookback_window + prediction_window))
+
+# X = np.empty((output_size, lookback_window, raw_data.shape[1]))
+# Y = np.empty((output_size, prediction_window, raw_data.shape[1]))
+X = np.empty((output_size, lookback_window, raw_data.shape[1]))
+Y = np.empty((output_size, prediction_window, raw_data.shape[1]))
+
+step_size = lookback_window + prediction_window
+
+for i in range(output_size):
+    X[i] = raw_data[
+        i * step_size : i * step_size + lookback_window
+    ]  # shape: (lookback_window, 3)
+    Y[i] = raw_data[
+        i * step_size
+        + lookback_window : i * step_size
+        + lookback_window
+        + prediction_window
+    ]  # shape: (prediction_window, 3)
+
+# for i in range(
+#     lookback_window, raw_data.shape[0] - prediction_window + 1):
+#     X[i - lookback_window] = raw_data[
+#         i - lookback_window : i
+#     ]  # shape: (lookback_window, 3)
+#     Y[i - lookback_window] = raw_data[
+#         i : i + prediction_window
+#     ]  # shape: (prediction_window, 3)
+
+print(X.shape)
+print(Y.shape)
+
+np.save("data/earth_sciences_anemometer/training_data/worst_day_X_40_10.npy", X)
+np.save("data/earth_sciences_anemometer/training_data/worst_day_Y_40_10.npy", Y)
